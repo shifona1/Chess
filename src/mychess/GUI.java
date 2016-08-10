49fc213902db60c55f6114dfbea63d6bd4faea14
Shifona Garg <shivanikrishgarg@gmail.com>
2016-08-10 23:14:07 +0530

    The Gui is more advanced that gives dialog boxes insead of console outputs along with colour highlights

--- src/mychess/GUI.java
+++ src/mychess/GUI.java
@@ -13,14 +13,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.event.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class GUI extends JFrame implements MouseMotionListener, MouseListener 
 {
-    static int side=50;
+    static int side=70;
     static int offsetx=20;
     static int offsety=50;
     int highlighter_x=-1,highlighter_y=-1,click_x=-1,click_y=-1;
-    int prevx,prevy,newx,newy;
-    boolean willMove=false;
+    int prevx,prevy,newx,newy,opacity=150;
+    boolean willMove=false,valid=true;
     
     Board board;
     MyChess chess;
@@ -32,10 +34,10 @@ public class GUI extends JFrame implements MouseMotionListener, MouseListener
         setSize(8*side+40,8*side+70);
         addMouseMotionListener(this);
         addMouseListener(this);
-        setVisible(true);
-        
+        setVisible(true);       
             
     }
+    
     public void paint(Graphics f)
 	{
             super.paint(f);
@@ -50,6 +52,29 @@ public class GUI extends JFrame implements MouseMotionListener, MouseListener
                     else if(board.tiles[i][j].color.equals("White"))
                         f.setColor(Color.WHITE);
                     f.fillRect(j*side+offsetx,i*side+offsety,side,side);
+                    if(i==highlighter_x && j==highlighter_y)
+                    {
+                        f.setColor(new Color(255,255,0,80));
+                        f.fillRect(highlighter_y*side+offsetx,highlighter_x*side+offsety,side,side);
+                    }  
+                    if(willMove)
+                if(i==click_x && j==click_y)
+                {
+                    //willMove=true;
+                    f.setColor(new Color(0,0,255,100));
+                    f.fillRect(click_y*side+offsetx,click_x*side+offsety,side,side);
+                }
+                    final int newi=i,newj=j;
+                    if(!willMove && !valid)
+                        if(newi==click_x && newj==click_y)
+                    {
+                        f.setColor(new Color(255,0,0,opacity));
+                        f.fillRect(click_y*side+offsetx,click_x*side+offsety,side,side);
+                        valid=true;
+                        
+                    }
+                    
+                    
                         if(board.tiles[i][j].piece!=null)
                         {                            
                             f.drawImage(board.tiles[i][j].piece.img, j*side+25,i*side+50-2, side-10, side-10, null);
@@ -57,18 +82,8 @@ public class GUI extends JFrame implements MouseMotionListener, MouseListener
                         }
                    
                         }
-            if(highlighter_x>=0&& highlighter_x<8 && highlighter_y >=0 && highlighter_y <8)
-            {
-                f.setColor(new Color(255,255,0,80));
-                f.fillRect(highlighter_y*side+offsetx,highlighter_x*side+offsety,side,side);
-            }
-            if(willMove)
-            if(click_x>=0&& click_x<8 && click_y >=0 && click_y <8 && board.tiles[click_x][click_y].piece!=null)
-            {
-                willMove=true;
-                f.setColor(new Color(0,0,255,100));
-                f.fillRect(click_y*side+offsetx,click_x*side+offsety,side,side);
-            }
+            
+            
 //            else
 //            {
 //                
@@ -96,6 +111,7 @@ public class GUI extends JFrame implements MouseMotionListener, MouseListener
         {
             prevx=click_x;
             prevy=click_y;
+            if(board.tiles[click_x][click_y].piece!=null)
             willMove=true;
         }
         else
@@ -107,9 +123,29 @@ public class GUI extends JFrame implements MouseMotionListener, MouseListener
 
                 @Override
                 public void run() {
-                   chess.move(prevx, prevy, newx, newy);
+                   valid=chess.move(prevx, prevy, newx, newy);
+//                   new Thread(){
+//
+//                        @Override
+//                        public void run() {
+//                            try {
+//                                //   super.run(); //To change body of generated methods, choose Tools | Templates.
+//                                for(int i=1;i<=10;i++)
+//                                {                                 
+//                                  Thread.sleep(50);
+//                                  opacity-=15;
+//                                }
+//                            } catch (InterruptedException ex) {
+//                                Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+//                            }
+//                            valid=true;
+//                            }
+//                    }.start();
+                   
+                   
                }
             });
+      
          }
         //repaint (); 
     }
--- src/mychess/MyChess.java
+++ src/mychess/MyChess.java
@@ -12,6 +12,7 @@ package mychess;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 public class MyChess {
 
     GUI gui;
@@ -44,40 +45,45 @@ public class MyChess {
             move(prevx,prevy,newx,newy);
     }
     
-    void move(int prevx,int prevy,int newx,int newy)
+    boolean move(int prevx,int prevy,int newx,int newy)
     {
             
         if(!(prevx>=0 && newx>=0 && prevx<8 && newx<8 && prevy>=0 && newy>=0 && prevy>=0 && newy>=0 )) {
             System.out.println("Please enter the valid coordinates");;
-            return;
+            return false;
         }
         if(board.tiles[prevx][prevy].piece==null)
         {
             System.out.println("There is no piece to move! ");;
-            return;
+            return false;
         }
         if((isBlackTurn && board.tiles[prevx][prevy].piece.color.equals("White"))||(!isBlackTurn && board.tiles[prevx][prevy].piece.color.equals("Black")))
         {
-            System.out.println("You can move only your colour pieces");
-            return;
+            JOptionPane.showMessageDialog(null,"Its not your turn !");
+            return false;
         }
             if(!board.tiles[prevx][prevy].piece.isValid(board, prevx, prevy, newx, newy))
             {
                 System.out.println("Invalid");
-                 return;
+                 return false;
             }   
                 if(board.tiles[newx][newy].piece!=null && board.tiles[newx][newy].piece.getClass()==King.class)
                 {
                     System.out.println("Game Over");
-                    if(isBlackTurn)System.out.println("White wins... Congrats!!!");
-                    else System.out.println("Black wins... Congrats!!!");
-                    return;
+                    
+                    board.tiles[newx][newy].piece=board.tiles[prevx][prevy].piece;
+                System.out.println("Moved");
+                board.tiles[prevx][prevy].piece=null;
+                if(isBlackTurn)JOptionPane.showMessageDialog(null, "Black Wins..... Congrats !!!!");
+                    else JOptionPane.showMessageDialog(null, "White Wins..... Congrats !!!!");
+                isBlackTurn=!isBlackTurn;
+                    System.exit(0);
                 }
                 board.tiles[newx][newy].piece=board.tiles[prevx][prevy].piece;
                 System.out.println("Moved");
                 board.tiles[prevx][prevy].piece=null;
                 isBlackTurn=!isBlackTurn;
-    
+                return true;
     }
     
     
@@ -95,7 +101,7 @@ public class MyChess {
                 try {
                     while(true) {
                     chess.gui.repaint();
-                    sleep(100);
+                    sleep(200);
                     }
                 } catch (InterruptedException ex) {
                     Logger.getLogger(MyChess.class.getName()).log(Level.SEVERE, null, ex);
